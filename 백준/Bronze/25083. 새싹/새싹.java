7725911e4c2ed9b4832760e4b1ea3aae35b97a58
pintor.dev <pintor.dev@gmail.com>
2024-03-21 21:46:28 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13836 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        
+        StringBuilder sb = new StringBuilder()
+            .append("         ,r\'\"7").append('\n')
+            .append("r`-_   ,\'  ,/").append('\n')
+            .append(" \\. \". L_r\'").append('\n')
+            .append("   `~\\/").append('\n')
+            .append("      |").append('\n')
+            .append("      |");
+        
+        System.out.println(sb);
+    }
+}
