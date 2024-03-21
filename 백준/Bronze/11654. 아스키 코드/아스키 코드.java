40bdb303c1d1bc228edb21512809c1cf1e042ac3
pintor.dev <pintor.dev@gmail.com>
2024-03-21 14:56:31 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -2,7 +2,6 @@ import java.io.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println((int) br.readLine().charAt(0));
+        System.out.println(System.in.read());
     }
 }
