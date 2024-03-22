2f2f00a817db98908c54a5b06e4743e09a32be45
pintor.dev <pintor.dev@gmail.com>
2024-03-22 13:44:53 +0900

    [Bronze II] Title: 진법 변환, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -1,15 +1,37 @@
 import java.io.*;
+import java.util.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
+        char[] n = readChars();
+        int b = readInt();
 
-        System.out.println(Integer.parseInt(read(), Integer.parseInt(read())));
+        int f = 1, dec = 0;
+        for (int i = n.length - 1; i >= 0; i--) {
+            dec += f * toDec(n[i]);
+            f *= b;
+        }
+
+        System.out.println(dec);
     }
 
-    public static String read() throws IOException {
-        StringBuilder sb = new StringBuilder();
-        int c;
-        while ((c = System.in.read()) > 32) sb.append((char) c);
-        return sb.toString();
+    public static char[] readChars() throws IOException {
+        char[] c = new char[35];
+        int idx = 0, ch;
+        while ((ch = System.in.read()) > 32) c[idx++] = (char) ch;
+        return Arrays.copyOf(c, idx);
+    }
+
+    public static int readInt() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+
+    public static int toDec(char c) {
+        if (c >= 'A') return c - 55;
+        else return c - '0';
     }
 }
