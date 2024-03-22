8effe8788899e1e93e001199deff812f0c61c4c2
pintor.dev <pintor.dev@gmail.com>
2024-03-22 15:56:15 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 120 ms, Memory: 14048 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+
+        int a = read(), b = read(), v = read();
+        System.out.println((v - b - 1) / (a - b) + 1);
+    }
+
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
