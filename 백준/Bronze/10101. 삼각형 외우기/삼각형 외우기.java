d1486eff34568b68d9cdb1721eb1f17373a7e065
pintor.dev <pintor.dev@gmail.com>
2024-03-23 13:37:50 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 120 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+
+        int a = read(), b = read(), c = read();
+        int all = a + b + c;
+
+        if (all == 180) {
+            if (a == b && b == c) System.out.println("Equilateral");
+            else if (a == b || b == c || c == a) System.out.println("Isosceles");
+            else System.out.println("Scalene");
+        }
+        else System.out.println("Error");
+    }
+
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
