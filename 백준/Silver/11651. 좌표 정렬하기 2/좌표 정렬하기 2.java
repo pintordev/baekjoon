43a37a19e3d384ba00eb05ae626bad659d4160b9
pintor.dev <pintor.dev@gmail.com>
2024-03-23 20:37:48 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 480 ms, Memory: 28524 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+
+        int n = read();
+        int[][] code = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            code[i][0] = read();
+            code[i][1] = read();
+        }
+        Arrays.sort(code, (o1, o2) -> {
+            if (o1[1] == o2[1]) return o1[0] - o2[0];
+            return o1[1] - o2[1];
+        });
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(code[i][0]).append(' ')
+                    .append(code[i][1]).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean negative = n == 13;
+        if (negative) n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return negative ? ~n + 1 : n;
+    }
+}
