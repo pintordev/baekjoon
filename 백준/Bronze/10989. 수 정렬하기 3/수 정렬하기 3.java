2b03311d43f1806d4f3b6916359b0fbabe75c526
pintor.dev <pintor.dev@gmail.com>
2024-03-23 20:13:53 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2012 ms, Memory: 224492 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+
+        int n = read();
+        int[] num = new int[10001];
+        for (int i = 0; i < n; i++) num[read()]++;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < 10001; i++) {
+            while (num[i]-- > 0) sb.append(i).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
