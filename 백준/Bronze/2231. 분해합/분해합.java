6a003536cfa1ed94468bd3c7953b62e27d2e3bb8
pintor.dev <pintor.dev@gmail.com>
2024-03-23 15:46:49 +0900

    [Bronze II] Title: 분해합, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+
+        int n = read();
+        int len = (n + "").length(), con = 0;
+        for (int i = n - 9 * len; i < n; i++) {
+            if (isCon(i, n)) {
+                con = i;
+                break;
+            }
+        }
+        System.out.println(con);
+    }
+
+    private static boolean isCon(int i, int n) {
+        int sum = i;
+        while (i > 0) {
+            sum += i % 10;
+            i /= 10;
+        }
+        return sum == n;
+    }
+
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
