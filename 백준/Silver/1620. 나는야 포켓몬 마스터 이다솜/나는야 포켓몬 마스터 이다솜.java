c964d112e21e539ba4bd4dea9c1cbd1023675ffb
pintor.dev <pintor.dev@gmail.com>
2024-03-25 09:51:01 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 512 ms, Memory: 44972 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -8,20 +8,20 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
 
-        Map<String, Integer> nameNum = new HashMap<>();
-        Map<Integer, String> numName = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
+        String[] names = new String[n + 1];
         int idx = 1;
         while (n-- > 0) {
             String name = br.readLine();
-            nameNum.put(name, idx);
-            numName.put(idx++, name);
+            map.put(name, idx);
+            names[idx++] = name;
         }
 
         StringBuilder sb = new StringBuilder();
         while (m-- > 0) {
             String question = br.readLine();
-            if (question.charAt(0) < 65) sb.append(numName.get(Integer.parseInt(question))).append('\n');
-            else sb.append(nameNum.get(question)).append('\n');
+            if (question.charAt(0) < 65) sb.append(names[Integer.parseInt(question)]).append('\n');
+            else sb.append(map.get(question)).append('\n');
         }
         System.out.println(sb);
     }
