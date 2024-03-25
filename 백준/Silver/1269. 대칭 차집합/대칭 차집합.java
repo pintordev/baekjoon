0bb70a9931953041ec3f24e93c89a6c5ddf3f583
pintor.dev <pintor.dev@gmail.com>
2024-03-25 11:17:10 +0900

    [Silver IV] Title: 대칭 차집합, Time: 380 ms, Memory: 45940 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -4,21 +4,25 @@ import java.util.*;
 class Main {
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+        int n = read(), m = read();
         Set<Integer> A = new HashSet<>(), B = new HashSet<>();
 
-        st = new StringTokenizer(br.readLine());
-        while (n-- > 0) A.add(Integer.parseInt(st.nextToken()));
+        while (n-- > 0) A.add(read());
 
-        st = new StringTokenizer(br.readLine());
         while (m-- > 0) {
-            int i = Integer.parseInt(st.nextToken());
+            int i = read();
             if (A.contains(i)) A.remove(i);
             else B.add(i);
         }
 
         System.out.println(A.size() + B.size());
+    }
+
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
     }
 }
