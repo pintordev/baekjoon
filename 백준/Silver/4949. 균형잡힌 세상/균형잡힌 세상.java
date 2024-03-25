5dfe755e9ce5aef68897730484e7fd14f63ad337
pintor.dev <pintor.dev@gmail.com>
2024-03-25 17:15:21 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 240 ms, Memory: 19584 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            char[] c = br.readLine().toCharArray();
+            if (c[0] == '.') break;
+            Stack<Character> stack = new Stack<>();
+            for (char ch : c) {
+                if (ch == '(' || ch == '[') stack.add(ch);
+                else if (ch == ')') {
+                    if (!stack.isEmpty() && stack.peek() == '(') stack.pop();
+                    else stack.add(ch);
+                }
+                else if (ch == ']') {
+                    if (!stack.isEmpty() && stack.peek() == '[') stack.pop();
+                    else stack.add(ch);
+                }
+            }
+            sb.append(stack.size() == 0 ? "yes" : "no").append('\n');
+        }
+        System.out.println(sb);
+    }
+}
