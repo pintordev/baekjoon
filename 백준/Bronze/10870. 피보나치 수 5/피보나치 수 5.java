7c300b2b8ed77703a92a4b0b576612f55eb8796d
pintor.dev <pintor.dev@gmail.com>
2024-03-26 13:48:51 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -4,10 +4,13 @@ class Main {
     public static void main(String[] args) throws IOException {
 
         int n = read();
-        int[] f = new int[n + 1];
-        if (n > 0) f[1] = 1;
-        for (int i = 2; i <= n; i++) f[i] = f[i - 1] + f[i - 2];
-        System.out.println(f[n]);
+        System.out.println(fib(n));
+    }
+
+    public static int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return  1;
+        return fib(n - 1) + fib(n - 2);
     }
 
     public static int read() throws IOException {
