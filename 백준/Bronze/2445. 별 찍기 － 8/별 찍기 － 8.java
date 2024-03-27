96a5b3b29b99940536f8a1dcc39d95026b4dc157
pintor.dev <pintor.dev@gmail.com>
2024-03-27 11:26:45 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 128 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < 2 * n; i++) {
+            int star = n - Math.abs(n - i);
+            sb.append("*".repeat(star));
+            sb.append(" ".repeat(2 * (n - star)));
+            sb.append("*".repeat(star));
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+}
