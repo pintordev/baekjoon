6f875d3862b2779def69d2f0a16ccdcc11fe6ac7
pintor.dev <pintor.dev@gmail.com>
2024-03-27 14:45:28 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 128 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,14 +1,14 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.printf("*");
-            }
-            System.out.println();
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) sb.append("*".repeat(i)).append('\n');
+        System.out.println(sb);
     }
 }
