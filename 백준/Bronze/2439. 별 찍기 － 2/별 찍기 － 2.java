935187498834e0befcb27fab0055da8965859303
pintor.dev <pintor.dev@gmail.com>
2024-03-27 10:35:00 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,9 +1,9 @@
-import java.util.*;
+import java.io.*;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= n; i++) {
