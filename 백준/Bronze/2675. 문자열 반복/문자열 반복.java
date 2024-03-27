c1b40b9cbc78cbf2ae158d41522d524ebb092297
pintor.dev <pintor.dev@gmail.com>
2024-03-27 12:01:07 +0900

    [Bronze II] Title: 문자열 반복, Time: 112 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,24 +1,24 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-        
+
         StringBuilder sb = new StringBuilder();
         while (t-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int r = Integer.parseInt(st.nextToken());
             char[] c = st.nextToken().toCharArray();
             for (int i = 0; i < c.length; i++) {
-                for (int j = 0; j < r; j++) {
-                    sb.append(c[i]);
-                }
+                for (int j = 0; j < r; j++) sb.append(c[i]);
             }
             sb.append('\n');
         }
-        
+
         System.out.println(sb);
     }
 }
