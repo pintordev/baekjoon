a0fe52588c51c12331983f8cf0d54c0525f74256
pintor.dev <pintor.dev@gmail.com>
2024-03-27 14:35:43 +0900

    [Silver II] Title: 참외밭, Time: 128 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Silver/2477. 참외밭/참외밭.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine());
+
+        int[] dir = new int[6];
+        int[] len = new int[6];
+        for (int i = 0; i < 6; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            dir[i] = Integer.parseInt(st.nextToken());
+            len[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int area = 0;
+        for (int i = 0; i < 6; i++) {
+            if (dir[i] != dir[(i + 2) % 6]) continue;
+            if (dir[(i + 1) % 6] != dir[(i + 3) % 6]) continue;
+            area += len[(i + 4) % 6] * len[(i + 5) % 6];
+            area -= len[(i + 1) % 6] * len[(i + 2) % 6];
+            area *= k;
+            break;
+        }
+        System.out.println(area);
+    }
+}
