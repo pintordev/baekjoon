3d5b8433b9b23b0339a45ccb729d677ba55006b8
pintor.dev <pintor.dev@gmail.com>
2024-03-27 19:27:08 +0900

    [Silver III] Title: N과 M (3), Time: 576 ms, Memory: 19868 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+class Main {
+
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int[] seq;
+
+    public static void main(String[] args) throws IOException {
+
+        int n = read();
+        int m = read();
+
+        seq = new int[m];
+
+        sequence(n, m, 0);
+        bw.flush();
+    }
+
+    private static void sequence(int n, int m, int depth) throws IOException {
+        if (depth == m) {
+            for (int s : seq) {
+                bw.write(s + 48);
+                bw.write(' ');
+            }
+            bw.newLine();
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            seq[depth] = i + 1;
+            sequence(n, m, depth + 1);
+        }
+    }
+
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
