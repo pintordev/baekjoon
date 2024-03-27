256850378038ca7342b3f8e326ccca4c461cd178
pintor.dev <pintor.dev@gmail.com>
2024-03-27 19:24:20 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+class Main {
+
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static boolean[] visited;
+    public static int[] seq;
+
+    public static void main(String[] args) throws IOException {
+
+        int n = read();
+        int m = read();
+
+        seq = new int[m];
+        visited = new boolean[n];
+
+        sequence(n, m, 0, 0);
+        bw.flush();
+    }
+
+    private static void sequence(int n, int m, int depth, int start) throws IOException {
+        if (depth == m) {
+            for (int s : seq) {
+                bw.write(s + 48);
+                bw.write(' ');
+            }
+            bw.newLine();
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                seq[depth] = i + 1;
+                sequence(n, m, depth + 1, i + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
