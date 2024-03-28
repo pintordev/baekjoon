45c4ab38ef734f9fee6fe5bc8737bfa77f18c298
pintor.dev <pintor.dev@gmail.com>
2024-03-28 11:27:50 +0900

    [Bronze II] Title: 단어의 개수, Time: 144 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,14 +1,20 @@
-import java.io.*;
+import java.io.IOException;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        int c, p = System.in.read(), count = 0;
+
+        int cnt = 0;
+        int p = System.in.read();
+        int c;
         while (true) {
             c = System.in.read();
-            if (p != 32 && (c == 32 || c == 10)) count++;
-            if (c == 10) break;
+            if (c == 32 && p != 32) cnt++;
+            if (c == 10) {
+                if (p != 32) cnt++;
+                break;
+            } 
             p = c;
         }
-        System.out.println(count);
+        System.out.println(cnt);
     }
 }
