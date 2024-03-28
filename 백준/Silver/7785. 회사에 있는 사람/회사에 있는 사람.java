e1c08f8c3aba356862b6af63942e3ec4836b75a5
pintor.dev <pintor.dev@gmail.com>
2024-03-28 13:54:05 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 576 ms, Memory: 45000 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -3,21 +3,20 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        Set<String> set = new TreeSet<>(Comparator.reverseOrder());
+        Set<String> inOffice = new TreeSet<>(Comparator.reverseOrder());
+        StringTokenizer st;
         while (n-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             String name = st.nextToken();
-            boolean io = st.nextToken().equals("enter");
-            if (io) set.add(name);
-            else set.remove(name);
+            if (st.nextToken().equals("enter")) inOffice.add(name);
+            else inOffice.remove(name);
         }
 
         StringBuilder sb = new StringBuilder();
-        for (String name : set) sb.append(name).append('\n');
+        for (String name : inOffice) sb.append(name).append('\n');
         System.out.println(sb);
     }
 }
