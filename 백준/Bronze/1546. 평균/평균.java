67c05683c3c483828fb93600bcbec11f9d48e4a9
pintor.dev <pintor.dev@gmail.com>
2024-03-28 13:39:23 +0900

    [Bronze I] Title: 평균, Time: 124 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,20 +1,21 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int max = 0;
-        double avg = 0;
-        for (int i = 0; i < n; i++) {
-            int score = Integer.parseInt(st.nextToken());
-            if (score > max) max = score;
-            avg += score;
-        }
 
-        System.out.println(avg / max * 100 / n);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum = 0;
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int m = Integer.parseInt(st.nextToken());
+            sum += m;
+            max = Math.max(max, m);
+        }
+        System.out.println((double) sum / max * 100 / n);
     }
 }
