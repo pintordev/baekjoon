4190baef8765186678742171d9a90f1302789bf7
pintor.dev <pintor.dev@gmail.com>
2024-03-29 13:46:55 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 268 ms, Memory: 28896 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -1,23 +1,20 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        Set<String> set = new HashSet<>();
-        int count = 0, enter = 0;
+        Set<String> inRoom = new HashSet<>();
+        int count = 0;
         while (n-- > 0) {
             String s = br.readLine();
-            if (s.equals("ENTER")) {
-                enter++;
-                set = new HashSet<>();
-            } else if (enter > 0 && !set.contains(s)) {
-                count++;
-                set.add(s);
-            }
+            if (s.equals("ENTER")) inRoom = new HashSet<>();
+            else if (inRoom.add(s)) count++;
         }
         System.out.println(count);
     }
