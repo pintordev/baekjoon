fd07174e538132339a26ae33dadf37f2a2f03b66
pintor.dev <pintor.dev@gmail.com>
2024-03-29 11:36:28 +0900

    [Silver III] Title: N과 M (1), Time: 204 ms, Memory: 17756 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,50 +1,45 @@
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.StringTokenizer;
 
 class Main {
 
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int n;
+    public static int m;
+    public static int[] permutation;
     public static boolean[] visited;
-    public static int[] seq;
 
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        int n = read();
-        int m = read();
-
-        seq = new int[m];
+        permutation = new int[m];
         visited = new boolean[n];
 
-        sequence(n, m, 0);
+        permute(0);
         bw.flush();
     }
 
-    private static void sequence(int n, int m, int d) throws IOException {
-        if (d == m) {
-            for (int s : seq) {
-                bw.write(s + 48);
+    public static void permute(int depth) throws IOException {
+
+        if (depth == m) {
+            for (int num : permutation) {
+                bw.write(num + 48);
                 bw.write(' ');
             }
-            bw.newLine();
+            bw.append('\n');
             return;
         }
 
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                seq[d] = i + 1;
-                sequence(n, m, d + 1);
+                permutation[depth] = i + 1;
+                permute(depth + 1);
                 visited[i] = false;
             }
         }
-    }
-
-    public static int read() throws IOException {
-        int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) > 32) {
-            n = (n << 3) + (n << 1) + (c & 15);
-        }
-        return n;
     }
 }
