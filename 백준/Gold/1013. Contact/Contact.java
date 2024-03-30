e43a6f0d6c872e5171d54b3949881185d779c497
pintor.dev <pintor.dev@gmail.com>
2024-03-30 14:58:53 +0900

    [Gold V] Title: Contact, Time: 148 ms, Memory: 15024 KB -BaekjoonHub

--- 백준/Gold/1013. Contact/Contact.java
+++ 백준/Gold/1013. Contact/Contact.java
@@ -1,27 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
 
+        Pattern pattern = Pattern.compile("(100+1+|01)+");
         StringBuilder sb = new StringBuilder();
-
-        String reg = "^(100+1+|01)+$";
-        Pattern pattern = Pattern.compile(reg);
-
-        for (int i = 0; i < T; i++) {
-            if (pattern.matcher(br.readLine()).matches()) {
-                sb.append("YES").append('\n');
-            } else {
-                sb.append("NO").append('\n');
-            }
+        while (t-- > 0) {
+            if (pattern.matcher(br.readLine()).matches()) sb.append("YES").append('\n');
+            else sb.append("NO").append('\n');
         }
-
         System.out.println(sb);
     }
 }
