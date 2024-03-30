62da9da3d6d04cb6853ba94030450869c4359bfc
pintor.dev <pintor.dev@gmail.com>
2024-03-30 18:09:29 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 128 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -9,25 +9,22 @@ class Main {
         while ((c = System.in.read()) != 10) chars[c - 65]++;
 
         StringBuilder sb = new StringBuilder();
-        boolean canMake = true;
         char hasOdd = 0;
         for (int i = 0; i < 26; i++) {
             char ch = (char) (i + 65);
-            if (chars[i] == 0) continue;
             sb.append((ch + "").repeat(chars[i] / 2));
+            
             if (chars[i] % 2 == 0) continue;
+            
             if (hasOdd == 0) hasOdd = ch;
             else {
-                canMake = false;
-                break;
+                System.out.println("I'm Sorry Hansoo");
+                return;
             }
         }
 
-        if (!canMake) System.out.println("I'm Sorry Hansoo");
-        else {
-            System.out.print(sb);
-            if (hasOdd != 0) System.out.print(hasOdd);
-            System.out.println(sb.reverse());
-        }
+        System.out.print(sb);
+        if (hasOdd != 0) System.out.print(hasOdd);
+        System.out.println(sb.reverse());
     }
 }
