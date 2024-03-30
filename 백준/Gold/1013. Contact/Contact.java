9f075afcea31e505c387d533136e6d8765497bf3
pintor.dev <pintor.dev@gmail.com>
2024-03-30 14:55:20 +0900

    [Gold V] Title: Contact, Time: 148 ms, Memory: 15024 KB -BaekjoonHub

---
+++ 백준/Gold/1013. Contact/Contact.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        String reg = "^(100+1+|01)+$";
+        Pattern pattern = Pattern.compile(reg);
+
+        for (int i = 0; i < T; i++) {
+            if (pattern.matcher(br.readLine()).matches()) {
+                sb.append("YES").append('\n');
+            } else {
+                sb.append("NO").append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
