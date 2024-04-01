741a36f6ce61521f803358f16a101530103bfbab
pintor.dev <pintor.dev@gmail.com>
2024-04-01 10:39:26 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 128 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,61 +1,47 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 class Main {
+
+    public static int n;
+    public static int m;
+    public static char[][] board;
+    public static int min;
+
     public static void main(String[] args) throws IOException {
 
-        int n = read(), m = read();
-        char[][] board = new char[n][m];
-        for (int i = 0; i < n; i++) {
-            board[i] = readChars();
-        }
-        char[] w = "WBWBWBWB".toCharArray(), b = "BWBWBWBW".toCharArray();
-        char[][] wBoard = new char[8][8];
-        char[][] bBoard = new char[8][8];
-        for (int i = 0; i < 8; i++) {
-            if (i % 2 == 0) {
-                wBoard[i] = w;
-                bBoard[i] = b;
-            } else {
-                wBoard[i] = b;
-                bBoard[i] = w;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+
+        board = new char[n][m];
+        for (int i = 0; i < n; i++) board[i] = br.readLine().toCharArray();
+
+        min = n * m;
+        for (int i = 0; i <= n - 8; i++) {
+            for (int j = 0; j <= m - 8; j++) {
+                repaint(i, j);
             }
         }
 
-        int min = 64;
-        for (int i = 0; i <= n - 8; i++) {
-            for (int j = 0; j <= m - 8; j++) {
-                min = Math.min(min, change(board, i, j, wBoard, bBoard));
-            }
-        }
         System.out.println(min);
     }
 
-    private static int change(char[][] board, int si, int sj, char[][] wBoard, char[][] bBoard) {
-        int wChange = 0, bChange = 0;
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                if (board[i + si][j + sj] != wBoard[i][j]) wChange++;
-                if (board[i + si][j + sj] != bBoard[i][j]) bChange++;
+    public static void repaint(int si, int sj) {
+
+        int w = 0;
+        int b = 0;
+        for (int i = si; i < si + 8; i++) {
+            for (int j = sj; j < sj + 8; j++) {
+                int k = (i + j - si - sj) % 2;
+                if (board[i][j] == 'W' && k == 0) b++;
+                else if (board[i][j] == 'W' && k == 1) w++;
+                else if (board[i][j] == 'B' && k == 1) b++;
+                else w++;
             }
         }
-        return Math.min(wChange, bChange);
-    }
-
-    public static char[] readChars() throws IOException {
-        char[] c = new char[50];
-        int idx = 0, ch;
-        while ((ch = System.in.read()) > 32) c[idx++] = (char) ch;
-        return Arrays.copyOf(c, idx);
-    }
-
-    public static int read() throws IOException {
-        int c, n = System.in.read() & 15;
-        boolean negative = n == 13;
-        if (negative) n = System.in.read() & 15;
-        while ((c = System.in.read()) > 32) {
-            n = (n << 3) + (n << 1) + (c & 15);
-        }
-        return negative ? ~n + 1 : n;
+        min = Math.min(min, Math.min(w, b));
     }
 }
