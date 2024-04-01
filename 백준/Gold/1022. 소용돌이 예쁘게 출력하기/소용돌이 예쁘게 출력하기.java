ade392f39b8d92e0297921929eccc65780d981a4
pintor.dev <pintor.dev@gmail.com>
2024-04-01 18:00:28 +0900

    [Gold III] Title: 소용돌이 예쁘게 출력하기, Time: 128 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Gold/1022. 소용돌이 예쁘게 출력하기/소용돌이 예쁘게 출력하기.java
+++ 백준/Gold/1022. 소용돌이 예쁘게 출력하기/소용돌이 예쁘게 출력하기.java
@@ -3,31 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Main {
-
-    public static int r1, c1, r2, c2;
-    public static int[][] tornado;
-    public static int max = 0, n = 0;
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
-        r1 = Integer.parseInt(input[0]);
-        c1 = Integer.parseInt(input[1]);
-        r2 = Integer.parseInt(input[2]);
-        c2 = Integer.parseInt(input[3]);
+        int r1 = Integer.parseInt(input[0]);
+        int c1 = Integer.parseInt(input[1]);
+        int r2 = Integer.parseInt(input[2]);
+        int c2 = Integer.parseInt(input[3]);
 
-        tornado = new int[r2 - r1 + 1][c2 - c1 + 1];
-
-        int limit = Math.max(Math.max(Math.abs(r1), Math.abs(r2)), Math.max(Math.abs(c1), Math.abs(c2)));
-        int r = 0, c = 0;
-        write(r, c);
-        for (int i = 1; i <= limit; i++) {
-            write(r, ++c);
-            for (int j = 0; j < 2 * i - 1; j++) write(--r, c);
-            for (int j = 0; j < 2 * i; j++) write(r, --c);
-            for (int j = 0; j < 2 * i; j++) write(++r, c);
-            for (int j = 0; j < 2 * i; j++) write(r, ++c);
+        int[][] tornado = new int[r2 - r1 + 1][c2 - c1 + 1];
+        int max = 0;
+        for (int i = r1; i <= r2; i++) {
+            for (int j = c1; j <= c2; j++) {
+                int order = Math.max(Math.abs(i), Math.abs(j));
+                int end = (2 * order + 1) * (2 * order + 1);
+                int idx = i - r1, jdx = j - c1;
+                if (i == order) tornado[idx][jdx] = end + j - order;
+                else if (j == -1 * order) tornado[idx][jdx] = end - 2 * order + i - order;
+                else if (i == -1 * order) tornado[idx][jdx] = end - 4 * order - (j + order);
+                else if (j == order) tornado[idx][jdx] = end - 6 * order - (i + order);
+                max = Math.max(max, tornado[idx][jdx]);
+            }
         }
 
         int maxLen = String.valueOf(max).length();
@@ -41,13 +38,5 @@ class Main {
             sb.append('\n');
         }
         System.out.println(sb);
-    }
-
-    public static void write(int r, int c) {
-        ++n;
-        if (r1 <= r && r <= r2 && c1 <= c && c <= c2) {
-            tornado[r - r1][c - c1] = n;
-            max = Math.max(max, n);
-        }
     }
 }
