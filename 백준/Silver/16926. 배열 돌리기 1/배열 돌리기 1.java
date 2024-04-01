1a8fa1691954f05bf0fd1b1db20d61376e4bd93d
pintor.dev <pintor.dev@gmail.com>
2024-04-01 12:50:48 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 504 ms, Memory: 38492 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -1,31 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Main {
 
     public static int n;
     public static int m;
+    public static int r;
     public static int[][] matrix;
     public static int limit;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        int r = Integer.parseInt(st.nextToken());
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+        r = Integer.parseInt(input[2]);
 
         matrix = new int[n][m];
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) matrix[i][j] = Integer.parseInt(st.nextToken());
+            input = br.readLine().split(" ");
+            for (int j = 0; j < m; j++) matrix[i][j] = Integer.parseInt(input[j]);
         }
 
         limit = Math.min(n, m) / 2;
-        for (int i = 0; i < r; i++) rotate();
+        rotate();
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
@@ -38,18 +40,16 @@ class Main {
     public static void rotate() {
         for (int k = 0; k < limit; k++) {
             int si = k, sj = k, ei = n - k - 1, ej = m - k - 1;
-            int mode = 1, hold = matrix[si][sj], i = si, j = sj;
-
-            while (mode < 5) {
-                if (mode == 1 && ++i == ei) mode++;
-                else if (mode == 2 && ++j == ej) mode++;
-                else if (mode == 3 && --i == si) mode++;
-                else if (mode == 4 && --j == sj) mode++;
-
-                int temp = matrix[i][j];
-                matrix[i][j] = hold;
-                hold = temp;
-            }
+            Queue<Integer> queue = new LinkedList<>();
+            for (int j = sj; j < ej; j++) queue.add(matrix[si][j]);
+            for (int i = si; i < ei; i++) queue.add(matrix[i][ej]);
+            for (int j = ej; j > sj; j--) queue.add(matrix[ei][j]);
+            for (int i = ei; i > si; i--) queue.add(matrix[i][sj]);
+            for (int i = 0; i < r; i++) queue.add(queue.poll());
+            for (int j = sj; j < ej; j++) matrix[si][j] = queue.poll();
+            for (int i = si; i < ei; i++) matrix[i][ej] = queue.poll();
+            for (int j = ej; j > sj; j--) matrix[ei][j] = queue.poll();
+            for (int i = ei; i > si; i--) matrix[i][sj] = queue.poll();
         }
     }
 }
