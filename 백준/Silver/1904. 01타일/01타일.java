3d0cbec9e2389f7726d65fbefff4987967045273
pintor.dev <pintor.dev@gmail.com>
2024-04-02 15:22:08 +0900

    [Silver III] Title: 01타일, Time: 132 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -10,11 +10,12 @@ class Main {
             return;
         }
 
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) dp[i] = (dp[i - 1] % mod + dp[i - 2] % mod) % mod;
-        System.out.println(dp[n]);
+        int a = 1, b = 2, c = 0;
+        for (int i = 3; i <= n; i++) {
+            c = (a + b) % mod;
+            a = b; b = c;
+        }
+        System.out.println(c);
     }
 
     public static int read() throws IOException {
