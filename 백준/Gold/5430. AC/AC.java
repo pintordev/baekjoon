89681c6e4ffd2703e6f6b8441abbdfcbbfdb54b6
pintor.dev <pintor.dev@gmail.com>
2024-04-03 13:57:53 +0900

    [Gold V] Title: AC, Time: 828 ms, Memory: 71496 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayDeque;
 import java.util.regex.Pattern;
 
 class Main {
@@ -9,33 +8,38 @@ class Main {
         int t = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
-        ArrayDeque<String> deque = new ArrayDeque<>();
         Pattern pattern = Pattern.compile("\\[|,|\\]");
         while (t-- > 0) {
             char[] commands = br.readLine().toCharArray();
             int n = Integer.parseInt(br.readLine());
             String[] numbers = pattern.split(br.readLine());
-            for (int i = 1; i <= n; i++) deque.add(numbers[i]);
             int commandsLen = commands.length;
             boolean dirFlag = true, isError = false;
+            int start = 1, end = numbers.length - 1;
             for (int i = 0; i < commandsLen; i++) {
                 if (commands[i] == 'R') dirFlag = !dirFlag;
                 else {
-                    if (deque.isEmpty()) {
+                    if (start > end) {
                         sb.append("error\n");
                         isError = true;
                         break;
                     }
-                    if (dirFlag) deque.pollFirst();
-                    else deque.pollLast();
+                    if (dirFlag) start++;
+                    else end--;
                 }
             }
             if (isError) continue;
             sb.append('[');
-            while (!deque.isEmpty()) {
-                if (dirFlag) sb.append(deque.pollFirst());
-                else sb.append(deque.pollLast());
-                if (!deque.isEmpty()) sb.append(',');
+            if (dirFlag) {
+                for (int i = start; i <= end; i++) {
+                    sb.append(numbers[i]);
+                    if (i < end) sb.append(',');
+                }
+            } else {
+                for (int i = end; i >= start; i--) {
+                    sb.append(numbers[i]);
+                    if (i > start) sb.append(',');
+                }
             }
             sb.append(']').append('\n');
         }
