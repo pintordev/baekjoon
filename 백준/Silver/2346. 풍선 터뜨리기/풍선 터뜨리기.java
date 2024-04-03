4d6887f5fea4ef2788e7885cd831e65574403ed2
pintor.dev <pintor.dev@gmail.com>
2024-04-03 10:46:07 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 124 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -1,40 +1,42 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 class Main {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        int n = read();
-        int[] num = new int[n];
-        for (int i = 0; i < n; i++) num[i] = read();
-
-        boolean[] pop = new boolean[n];
+        List<Balloon> balloons = new ArrayList<>();
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) balloons.add(new Balloon(i + 1, Integer.parseInt(input[i])));
+        
         StringBuilder sb = new StringBuilder();
-        int pdx = 0, k = 0;
-        for (int i = 0; i < n; i++) {
-            while (Math.abs(k) > 0) {
-                if (k < 0) {
-                    pdx = (pdx - 1 + n) % n;
-                    if (!pop[pdx]) k++;
-                } else {
-                    pdx = (pdx + 1) % n;
-                    if (!pop[pdx]) k--;
-                }
-            }
-            pop[pdx] = true;
-            sb.append(pdx + 1).append(' ');
-            k = num[pdx];
+        int idx = 0;
+        while (true) {
+            Balloon balloon = balloons.get(idx);
+            sb.append(balloon.idx).append(' ');
+
+            balloons.remove(idx);
+            if (balloons.isEmpty()) break;
+
+            int move = balloon.move;
+            int size = balloons.size();
+            if (move > 0) move--;
+            idx = (idx + move) % size;
+            while (idx < 0 ) idx += size;
         }
         System.out.println(sb);
     }
+}
 
-    public static int read() throws IOException {
-        int c, n = System.in.read() & 15;
-        boolean negative = n == 13;
-        if (negative) n = System.in.read() & 15;
-        while ((c = System.in.read()) > 32) {
-            n = (n << 3) + (n << 1) + (c & 15);
-        }
-        return negative ? ~n + 1 : n;
+class Balloon {
+    int idx;
+    int move;
+
+    public Balloon(int idx, int move) {
+        this.idx = idx;
+        this.move = move;
     }
 }
