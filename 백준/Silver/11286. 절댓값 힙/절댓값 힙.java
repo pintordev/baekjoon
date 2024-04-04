27c65b01d9629ef4986ee70395226cfdb578cf7f
pintor.dev <pintor.dev@gmail.com>
2024-04-04 10:23:36 +0900

    [Silver I] Title: 절댓값 힙, Time: 332 ms, Memory: 25736 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,20 +1,31 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        
+
         PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
-            if (Math.abs(o1) == Math.abs(o2)) return o1 - o2;
-            else return Math.abs(o1) - Math.abs(o2);
+            int abs1 = Math.abs(o1);
+            int abs2 = Math.abs(o2);
+            if (abs1 == abs2) return o1 - o2;
+            return abs1 - abs2;
         });
+
+        StringBuilder sb = new StringBuilder();
         while (n-- > 0) {
             int x = Integer.parseInt(br.readLine());
-            if (x != 0) pq.add(x);
-            else if (!pq.isEmpty()) System.out.println(pq.poll());
-            else System.out.println("0");
+            if (x != 0) {
+                pq.add(x);
+            } else if (!pq.isEmpty()) {
+                sb.append(pq.poll()).append('\n');
+            } else {
+                sb.append(0).append('\n');
+            }
         }
+        System.out.println(sb);
     }
 }
