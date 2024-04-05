49e95120527f3ffe4cb57c72023266cd10a0ed69
pintor.dev <pintor.dev@gmail.com>
2024-04-05 09:55:45 +0900

    [Silver II] Title: 나무 자르기, Time: 304 ms, Memory: 18324 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,19 +1,14 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
+        int n = read();
+        int m = read();
 
-        input = br.readLine().split(" ");
         int high = 0;
         int[] woods = new int[n];
         for (int i = 0; i < n; i++) {
-            woods[i] = Integer.parseInt(input[i]);
+            woods[i] = read();
             high = Math.max(high, woods[i]);
         }
 
@@ -36,5 +31,13 @@ class Main {
         }
 
         System.out.println(high);
+    }
+    
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
     }
 }
