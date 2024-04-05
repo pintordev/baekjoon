8beadd5977306214edb21616aa593ad5e6748e61
pintor.dev <pintor.dev@gmail.com>
2024-04-05 19:54:59 +0900

    [Silver IV] Title: ATM, Time: 120 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -4,22 +4,23 @@ import java.util.Arrays;
 class Main {
     public static void main(String[] args) throws IOException {
         int n = read();
+
         int[] p = new int[n];
-        int[] pp = new int[n];
         for (int i = 0; i < n; i++) {
             p[i] = read();
         }
+        
         Arrays.sort(p);
 
-        int sum = p[0];
-        pp[0] = p[0];
+        int take = p[0];
         for (int i = 1; i < n; i++) {
-            pp[i] = pp[i - 1] + p[i];
-            sum += pp[i];
+            p[i] += p[i - 1];
+            take += p[i];
         }
-        System.out.println(sum);
-    }
 
+        System.out.println(take);
+    }
+    
     public static int read() throws IOException {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) > 32) {
