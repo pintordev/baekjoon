d5fa9a41290927f95afcda839e6e25484e3e1988
pintor.dev <pintor.dev@gmail.com>
2024-04-06 15:38:55 +0900

    [Gold II] Title: 가운데를 말해요, Time: 468 ms, Memory: 30696 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -1,22 +1,19 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int n = read();
 
         StringBuilder sb = new StringBuilder();
         PriorityQueue<Integer> leftQ = new PriorityQueue<>(Comparator.reverseOrder());
         PriorityQueue<Integer> rightQ = new PriorityQueue<>();
         while (n-- > 0) {
             if (leftQ.size() == rightQ.size()) {
-                leftQ.add(Integer.parseInt(br.readLine()));
+                leftQ.add(read());
             } else {
-                rightQ.add(Integer.parseInt(br.readLine()));
+                rightQ.add(read());
             }
 
             while (!rightQ.isEmpty() && leftQ.peek() > rightQ.peek()) {
@@ -29,5 +26,15 @@ class Main {
             sb.append(leftQ.peek()).append('\n');
         }
         System.out.println(sb);
+    }
+    
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean negative = n == 13;
+        if (negative) n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return negative ? ~n + 1 : n;
     }
 }
