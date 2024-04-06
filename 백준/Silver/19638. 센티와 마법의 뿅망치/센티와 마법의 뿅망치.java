6c360b44635e44e780a4f955763db3c893089bd2
pintor.dev <pintor.dev@gmail.com>
2024-04-06 11:56:30 +0900

    [Silver I] Title: 센티와 마법의 뿅망치, Time: 288 ms, Memory: 20928 KB -BaekjoonHub

--- 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.java
+++ 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.java
@@ -1,20 +1,16 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int h = Integer.parseInt(input[1]);
-        int t = Integer.parseInt(input[2]);
+        int n = read();
+        int h = read();
+        int t = read();
 
         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
         while (n-- > 0) {
-            pq.add(Integer.parseInt(br.readLine()));
+            pq.add(read());
         }
 
         int cnt = 0;
@@ -35,5 +31,13 @@ class Main {
             sb.append("YES").append('\n').append(cnt);
         }
         System.out.println(sb);
+    }
+    
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
     }
 }
