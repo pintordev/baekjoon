076ca6e24104ae83ff30761578e6e36e1cff2eb8
pintor.dev <pintor.dev@gmail.com>
2024-04-06 15:14:17 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 336 ms, Memory: 25500 KB -BaekjoonHub

--- 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.java
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.java
@@ -1,38 +1,32 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.TreeSet;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input;
-
         TreeSet<Problem> problems = new TreeSet<>();
-        int n = Integer.parseInt(br.readLine());
+        int n = read();
         while (n-- > 0) {
-            input = br.readLine().split(" ");
-            problems.add(new Problem(Integer.parseInt(input[0]), Integer.parseInt(input[1])));
+            problems.add(new Problem(read(), read()));
         }
 
         StringBuilder sb = new StringBuilder();
         HashSet<Integer> solved = new HashSet<>();
-        int m = Integer.parseInt(br.readLine());
+        int m = read();
         while (m-- > 0) {
-            input = br.readLine().split(" ");
-            if (input[0].equals("recommend")) {
-                if (input[1].equals("1")) {
+            String cmd = readString();
+            if (cmd.equals("recommend")) {
+                if (read() == 1) {
                     sb.append(problems.last().number).append('\n');
                 } else {
                     sb.append(problems.first().number).append('\n');
                 }
-            } else if (input[0].equals("add")) {
-                Problem p = new Problem(Integer.parseInt(input[1]), Integer.parseInt(input[2]));
+            } else if (cmd.equals("add")) {
+                Problem p = new Problem(read(), read());
                 problems.add(p);
                 solved.remove(p.number);
             } else {
-                solved.add(Integer.parseInt(input[1]));
+                solved.add(read());
                 while (!problems.isEmpty() && solved.contains(problems.last().number)) {
                     problems.pollLast();
                 }
@@ -43,6 +37,25 @@ class Main {
         }
 
         System.out.println(sb);
+    }
+    
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean negative = n == 13;
+        if (negative) n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return negative ? ~n + 1 : n;
+    }
+    
+    public static String readString() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int c;
+        while ((c = System.in.read()) > 32) {
+            sb.append((char) c);
+        }
+        return sb.toString();
     }
 }
 
