46d14e767a30458007b3063dc54c66f4b57a64e2
pintor.dev <pintor.dev@gmail.com>
2024-04-09 09:17:09 +0900

    [Silver I] Title: 숨바꼭질, Time: 152 ms, Memory: 16484 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,40 +1,42 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 
 class Main {
+    public static int[] visited = new int[100001];
+    public static Queue<Integer> queue = new ArrayDeque<>();
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int k = Integer.parseInt(input[1]);
+        int n = read();
+        int k = read();
 
-        int[] visited = new int[100001];
-        visited[n] = 1;
-
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(n);
+        goIfCanMove(n, n);
         while (!queue.isEmpty()) {
             int now = queue.poll();
             if (now == k) {
                 System.out.println(visited[now] - 1);
-                break;
+                return;
             }
 
-            if (now - 1 >= 0 && visited[now - 1] == 0) {
-                visited[now - 1] = visited[now] + 1;
-                queue.add(now - 1);
-            }
-            if (now + 1 <= 100000 && visited[now + 1] == 0) {
-                visited[now + 1] = visited[now] + 1;
-                queue.add(now + 1);
-            }
-            if (now * 2 <= 100000 && visited[now * 2] == 0) {
-                visited[now * 2] = visited[now] + 1;
-                queue.add(now * 2);
-            }
+            goIfCanMove(now - 1, now);
+            if (now >= k) continue;
+            goIfCanMove(2 * now, now);
+            goIfCanMove(now + 1, now);
         }
+    }
+
+    public static void goIfCanMove(int to, int from) {
+        if (to < 0 || to > 100000) return;
+        if (visited[to] > 0) return;
+        queue.add(to);
+        visited[to] = visited[from] + 1;
+    }
+
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
     }
 }
