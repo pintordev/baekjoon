b02b0d479a376afa09b8908876f750a3c3b7a9b0
pintor.dev <pintor.dev@gmail.com>
2024-04-10 10:54:23 +0900

    [Silver II] Title: 외판원 순회 2, Time: 348 ms, Memory: 14652 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static int n;
+    public static int[][] map;
+    public static boolean[] visited;
+    public static int minCost = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        map = new int[n][n];
+        String[] input;
+        for (int i = 0; i < n; i++) {
+            input = br.readLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+
+        visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            visited[i] = true;
+            dfs(i, i, 1, 0);
+            visited[i] = false;
+        }
+
+        System.out.println(minCost);
+    }
+
+    public static void dfs(int i, int start, int depth, int cost) {
+        if (depth == n) {
+            if (map[i][start] == 0) {
+                return;
+            }
+            minCost = Math.min(minCost, cost + map[i][start]);
+            return;
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (i == j || visited[j] || map[i][j] == 0) {
+                continue;
+            }
+            visited[j] = true;
+            dfs(j, start, depth + 1, cost + map[i][j]);
+            visited[j] = false;
+        }
+    }
+}
