8bd450bad489082b345b67dc5d8c2074eacb0804
pintor.dev <pintor.dev@gmail.com>
2024-04-12 11:26:30 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 244 ms, Memory: 20796 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
 public class Main {
@@ -11,7 +9,6 @@ public class Main {
     public static boolean[][] visited;
     public static int[] dx = {1, -1, 0, 0};
     public static int[] dy = {0, 0, 1, -1};
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
@@ -64,7 +61,7 @@ public class Main {
     }
 
     public static void init() throws IOException {
-        n = Integer.parseInt(br.readLine());
+        n = read();
         if (n == 0) {
             System.out.println(sb);
             System.exit(0);
@@ -75,12 +72,19 @@ public class Main {
         visited = new boolean[n][n];
 
         for (int i = 0; i < n; i++) {
-            String[] input = br.readLine().split(" ");
             for (int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(input[j]);
+                map[i][j] = read();
                 rupee[i][j] = Integer.MAX_VALUE;
             }
         }
+    }
+    
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
     }
 }
 
