2d581435575b9346d0ef0f53a064118821efce90
pintor.dev <pintor.dev@gmail.com>
2024-04-12 10:29:47 +0900

    [Gold V] Title: 행복 유치원, Time: 420 ms, Memory: 23572 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -1,35 +1,35 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int k = Integer.parseInt(input[1]);
+        int n = read();
+        int k = read();
 
         int[] arr = new int[n];
-        input = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(input[i]);
+            arr[i] = read();
         }
-
         Arrays.sort(arr);
 
         int[] diff = new int[n - 1];
         for (int i = 0; i < n - 1; i++) {
             diff[i] = arr[i + 1] - arr[i];
         }
-
         Arrays.sort(diff);
 
         int sum = 0;
         for (int i = 0; i < n - k; i++) {
             sum += diff[i];
         }
-        
         System.out.println(sum);
+    }
+    
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
     }
 }
