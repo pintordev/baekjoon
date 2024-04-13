90b297caa77412d53059ce7f402389a8f87ae6a2
pintor.dev <pintor.dev@gmail.com>
2024-04-13 12:10:55 +0900

    [Gold V] Title: 동전 1, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,22 +1,26 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int k = Integer.parseInt(input[1]);
+        int n = read();
+        int k = read();
 
         int[] memo = new int[k + 1];
         memo[0] = 1;
         for (int i = 0; i < n; i++) {
-            int coin = Integer.parseInt(br.readLine());
+            int coin = read();
             for (int j = coin; j <= k; j++) {
                 memo[j] += memo[j - coin];
             }
         }
         System.out.println(memo[k]);
+    }
+    
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
     }
 }
