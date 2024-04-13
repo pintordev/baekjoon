6cde32ed0b247723bfebd17379e119786b58e441
pintor.dev <pintor.dev@gmail.com>
2024-04-13 10:24:25 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 136 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -1,16 +1,12 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int n = read();
 
         int[] arr = new int[n];
-        String[] input = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(input[i]);
+            arr[i] = read();
         }
 
         int[] memo = new int[n];
@@ -25,5 +21,13 @@ public class Main {
             max = Math.max(max, memo[i]);
         }
         System.out.println(max);
+    }
+    
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
     }
 }
