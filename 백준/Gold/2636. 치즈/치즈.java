04b1849e1e5adbe7bdd2d1b76b9fb7e5e11a2141
pintor.dev <pintor.dev@gmail.com>
2024-04-15 11:09:59 +0900

    [Gold IV] Title: 치즈, Time: 160 ms, Memory: 15288 KB -BaekjoonHub

---
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Main {
+    public static int n;
+    public static int m;
+    public static int[][] board;
+    public static int cheese = 0;
+    public static boolean[][] visited;
+    public static int dr[] = {0, 0, 1, -1};
+    public static int dc[] = {1, -1, 0, 0};
+    public static int melted;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        simulate();
+    }
+
+    public static void simulate() {
+        int time = 0;
+        while (cheese > 0) {
+            time++;
+            bfs(0, 0);
+            cheese -= melted;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(time).append('\n').append(melted);
+        System.out.println(sb);
+    }
+
+    public static void bfs(int r, int c) {
+        visited = new boolean[n][m];
+        melted = 0;
+
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(new Node(r, c));
+        visited[r][c] = true;
+        while (!queue.isEmpty()) {
+            Node now = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nr = now.r + dr[i];
+                int nc = now.c + dc[i];
+                if (!canMove(nr, nc)) {
+                    continue;
+                }
+                visited[nr][nc] = true;
+
+                if (board[nr][nc] == 0) {
+                    queue.add(new Node(nr, nc));
+                } else {
+                    board[nr][nc] = 0;
+                    melted++;
+                }
+            }
+        }
+    }
+
+    public static boolean canMove(int r, int c) {
+        return r >= 0 && r < n && c >= 0 && c < m && !visited[r][c];
+    }
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+
+        board = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            input = br.readLine().split(" ");
+            for (int j = 0; j < m; j++) {
+                board[i][j] = Integer.parseInt(input[j]);
+                if (board[i][j] == 1) cheese++;
+            }
+        }
+    }
+}
+
+class Node {
+    int r;
+    int c;
+
+    public Node(int r, int c) {
+        this.r = r;
+        this.c = c;
+    }
+}
