3525c0065e7ffd943f51a80d2902dbe2468383a9
pintor.dev <pintor.dev@gmail.com>
2024-04-15 11:11:30 +0900

    [Gold IV] Title: 치즈, Time: 152 ms, Memory: 14752 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
@@ -65,19 +63,24 @@ public class Main {
     }
 
     public static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        n = Integer.parseInt(input[0]);
-        m = Integer.parseInt(input[1]);
+        n = read();
+        m = read();
 
         board = new int[n][m];
         for (int i = 0; i < n; i++) {
-            input = br.readLine().split(" ");
             for (int j = 0; j < m; j++) {
-                board[i][j] = Integer.parseInt(input[j]);
+                board[i][j] = read();
                 if (board[i][j] == 1) cheese++;
             }
         }
+    }
+    
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
     }
 }
 
