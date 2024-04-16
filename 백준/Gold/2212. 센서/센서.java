800a444d0740c9505a78079f010446cd71a93742
pintor.dev <pintor.dev@gmail.com>
2024-04-16 17:49:04 +0900

    [Gold V] Title: 센서, Time: 156 ms, Memory: 15280 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.java
+++ 백준/Gold/2212. 센서/센서.java
@@ -1,18 +1,14 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int k = Integer.parseInt(br.readLine());
+        int n = read();
+        int k = read();
 
         int[] arr = new int[n];
-        String[] input = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(input[i]);
+            arr[i] = read();
         }
 
         Arrays.sort(arr);
@@ -29,5 +25,15 @@ public class Main {
             sum += diff[i];
         }
         System.out.println(sum);
+    }
+
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean negative = n == 13;
+        if (negative) n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return negative ? ~n + 1 : n;
     }
 }
