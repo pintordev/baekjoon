010d433ff58450061f4ff730918ad9acc396aa43
pintor.dev <pintor.dev@gmail.com>
2024-04-16 09:47:31 +0900

    [Gold V] Title: 센서, Time: 192 ms, Memory: 17828 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.java
+++ 백준/Gold/2212. 센서/센서.java
@@ -2,33 +2,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int N = Integer.parseInt(br.readLine());
-		int K = Integer.parseInt(br.readLine());
-		
-		int arr[] = new int[N];
-		int diff[] = new int[N-1];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<N;i++) arr[i] = Integer.parseInt(st.nextToken());
-		
-		Arrays.sort(arr);
-		
-		for(int i=0;i<N-1;i++) diff[i] = arr[i+1]-arr[i];
-		
-		Arrays.sort(diff);
-		
-		int res = 0;
-		for(int i=0;i<N-K;i++) res += diff[i];
-		
-		System.out.println(res);
-	}
+        int[] arr = new int[n];
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+
+        Arrays.sort(arr);
+
+        int[] diff = new int[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            diff[i] = arr[i + 1] - arr[i];
+        }
+
+        Arrays.sort(diff);
+
+        int sum = 0;
+        for (int i = 0; i < n - k; i++) {
+            sum += diff[i];
+        }
+        System.out.println(sum);
+    }
 }
-
