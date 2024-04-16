d11ed653a11d098b5ddf97fbcaa6106e72fbb2da
pintor.dev <pintor.dev@gmail.com>
2024-04-16 09:10:12 +0900

    [Silver I] Title: 스티커, Time: 316 ms, Memory: 28244 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -2,33 +2,33 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        StringBuilder sb = new StringBuilder();
-
         int t = read();
+
+        StringBuilder sb = new StringBuilder();
         while (t-- > 0) {
             int n = read();
-            int[][] score = new int[2][n];
+
+            int[][] sticker = new int[2][n];
             for (int i = 0; i < 2; i++) {
                 for (int j = 0; j < n; j++) {
-                    score[i][j] = read();
+                    sticker[i][j] = read();
                 }
             }
 
             if (n == 1) {
-                System.out.println(Math.max(score[0][0], score[1][0]));
+                sb.append(Math.max(sticker[0][0], sticker[1][0])).append('\n');
                 continue;
             }
 
             int[][] memo = new int[2][n];
-            memo[0][0] = score[0][0];
-            memo[1][0] = score[1][0];
-            memo[0][1] = memo[1][0] + score[0][1];
-            memo[1][1] = memo[0][0] + score[1][1];
+            memo[0][0] = sticker[0][0];
+            memo[1][0] = sticker[1][0];
+            memo[0][1] = memo[1][0] + sticker[0][1];
+            memo[1][1] = memo[0][0] + sticker[1][1];
             for (int i = 2; i < n; i++) {
-                memo[0][i] = Math.max(memo[1][i - 1], memo[1][i - 2]) + score[0][i];
-                memo[1][i] = Math.max(memo[0][i - 1], memo[0][i - 2]) + score[1][i];
+                memo[0][i] = Math.max(memo[1][i - 2], memo[1][i - 1]) + sticker[0][i];
+                memo[1][i] = Math.max(memo[0][i - 2], memo[0][i - 1]) + sticker[1][i];
             }
-
             sb.append(Math.max(memo[0][n - 1], memo[1][n - 1])).append('\n');
         }
         System.out.println(sb);
