951c5f3d36ecbc054f4a7dde9a89aa725c6cd01d
pintor.dev <pintor.dev@gmail.com>
2024-08-29 02:41:54 +0900

    [Gold III] Title: 소수의 연속합, Time: 176 ms, Memory: 22696 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Main {
+    public static int n;
+    public static boolean[] np;
+
+    public static void main(String[] args) throws IOException {
+        n = read();
+
+        eratosthenes();
+        System.out.println(count());
+    }
+
+    public static void eratosthenes() {
+        np = new boolean[n + 1];
+        np[0] = np[1] = true;
+
+        int cnt = n - 1;
+        for (int i = 2; i * i <= n; i++) {
+            if (np[i]) continue;
+            for (int j = i << 1; j <= n; j += i) {
+                if (np[j]) continue;
+                np[j] = true;
+                cnt--;
+            }
+        }
+    }
+
+    public static int count() {
+        int cnt = 0;
+        int sum = 0;
+        Queue<Integer> q = new ArrayDeque<>();
+        for (int i = 2; i <= n; i++) {
+            if (np[i]) continue;
+            q.add(i);
+            sum += i;
+            while (sum > n) {
+                sum -= q.poll();
+            }
+            if (sum == n) cnt++;
+        }
+        return cnt;
+    }
+
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
