f13e993501c7a59d53269e77ecf5ecfb7433bf93
pintor.dev <pintor.dev@gmail.com>
2024-09-08 15:48:57 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 496 ms, Memory: 43472 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static int n;
+    public static List<Integer>[] tree;
+    public static int[] parent;
+    public static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        n = read();
+
+        tree = new List[n + 1];
+        for (int i = 1; i <= n; i++) {
+            tree[i] = new ArrayList<>();
+        }
+        for (int i = 1; i < n; i++) {
+            int a = read();
+            int b = read();
+            tree[a].add(b);
+            tree[b].add(a);
+        }
+
+        parent = new int[n + 1];
+        visited = new boolean[n + 1];
+        dfs(1);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= n; i++) {
+            sb.append(parent[i]).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static void dfs(int idx) {
+        visited[idx] = true;
+        for (int i : tree[idx]) {
+            if (visited[i]) continue;
+            parent[i] = idx;
+            dfs(i);
+        }
+    }
+
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
