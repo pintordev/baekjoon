2035752c6e2b0bc0ab9ebcf55275038962458fd6
pintor.dev <pintor.dev@gmail.com>
2024-09-14 18:33:34 +0900

    [Gold V] Title: 집합의 표현, Time: 232 ms, Memory: 24232 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+
+public class Main {
+    public static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        int n = read();
+        int m = read();
+
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            int op = read();
+            int a = read();
+            int b = read();
+            if (op == 0) union(a, b);
+            else sb.append(find(a) == find(b) ? "YES\n" : "NO\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static int find(int a) {
+        if (parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
+    }
+
+    public static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) parent[a] = b;
+    }
+
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
