ca33b7a35027a83c8e0c5364fcd8905edc6c501d
pintor.dev <pintor.dev@gmail.com>
2024-10-25 18:13:04 +0900

    [Platinum V] Title: 버블 소트, Time: 308 ms, Memory: 24552 KB -BaekjoonHub

--- 백준/Platinum/1517. 버블 소트/버블 소트.java
+++ 백준/Platinum/1517. 버블 소트/버블 소트.java
@@ -1,18 +1,58 @@
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Main {
+    public static int[] arr;
+    public static int[] temp;
+    public static long cnt;
+
     public static void main(String[] args) throws IOException {
         int n = read();
 
-        Node[] arr = new Node[n + 1];
-        for (int i = 1; i <= n; i++) {
-            arr[i] = new Node(i, read());
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = read();
         }
-        Arrays.sort(arr, 1, n + 1);
 
-        SegmentTree tree = new SegmentTree(n, arr);
-        System.out.println(tree.swap(n));
+        temp = new int[n];
+        cnt = 0;
+        mergeSort(0, n - 1);
+        System.out.println(cnt);
+    }
+
+    public static void mergeSort(int s, int e) {
+        if (s < e) {
+            int mid = (s + e) >> 1;
+            mergeSort(s, mid);
+            mergeSort(mid + 1, e);
+            merge(s, mid, e);
+        }
+    }
+
+    public static void merge(int s, int mid, int e) {
+        int i = s;
+        int j = mid + 1;
+        int k = s;
+
+        while (i <= mid && j <= e) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+                cnt += mid - i + 1;
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= e) {
+            temp[k++] = arr[j++];
+        }
+
+        for (int l = s; l <= e; l++) {
+            arr[l] = temp[l];
+        }
     }
 
     public static int read() throws IOException {
@@ -21,60 +61,5 @@ public class Main {
             n = (n << 3) + (n << 1) + (c & 15);
         }
         return n;
-    }
-}
-
-class SegmentTree {
-    public long[] tree;
-    public Node[] arr;
-
-    public SegmentTree(int n, Node[] arr) {
-        this.arr = arr;
-        int h = (int) Math.ceil(Math.log(n) / Math.log(2));
-        tree = new long[1 << (h + 1)];
-    }
-
-    public long swap(int n) {
-        long cnt = 0;
-        for (int i = 1; i <= n; i++) {
-            cnt += query(1, 1, n, arr[i].idx, n);
-            update(1, 1, n, arr[i].idx);
-        }
-        return cnt;
-    }
-
-    public long query(int cur, int s, int e, int l, int r) {
-        if (l > e || r < s) return 0;
-        if (l <= s && e <= r) return tree[cur];
-        int next = cur << 1;
-        int mid = (s + e) >> 1;
-        return query(next, s, mid, l, r) + query(next + 1, mid + 1, e, l, r);
-    }
-
-    public void update(int cur, int s, int e, int idx) {
-        if (s == e) {
-            tree[cur] = 1;
-            return;
-        }
-        int next = cur << 1;
-        int mid = (s + e) >> 1;
-        if (idx <= mid) update(next, s, mid, idx);
-        else update(next + 1, mid + 1, e, idx);
-        tree[cur] = tree[next] + tree[next + 1];
-    }
-}
-
-class Node implements Comparable<Node> {
-    public int idx;
-    public int val;
-
-    public Node(int idx, int val) {
-        this.idx = idx;
-        this.val = val;
-    }
-
-    @Override
-    public int compareTo(Node o) {
-        return this.val - o.val;
     }
 }
