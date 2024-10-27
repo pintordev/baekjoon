da77acb3c36c062c2a9aab88bfd37f07ce472a30
pintor.dev <pintor.dev@gmail.com>
2024-10-27 15:06:04 +0900

    [Gold III] Title: 수상 택시, Time: 728 ms, Memory: 32208 KB -BaekjoonHub

---
+++ 백준/Gold/2836. 수상 택시/수상 택시.java
@@ -0,0 +1,59 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int n = read();
+        int m = read();
+
+        List<Passenger> passengers = new ArrayList<>();
+        while (n-- > 0) {
+            int s = read();
+            int e = read();
+            if (s <= e) continue;
+            passengers.add(new Passenger(s, e));
+        }
+
+        if (passengers.isEmpty()) {
+            System.out.println(m);
+            return;
+        }
+
+        passengers.sort(Passenger::compareTo);
+
+        long dis = passengers.get(0).s - passengers.get(0).e;
+        int prev = passengers.get(0).s;
+        for (Passenger p : passengers) {
+            if (prev >= p.s) continue;
+            if (prev >= p.e) dis += p.s - prev;
+            else dis += p.s - p.e;
+            prev = p.s;
+        }
+        System.out.println(m + (dis << 1));
+    }
+
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
+
+class Passenger implements Comparable<Passenger> {
+    int s;
+    int e;
+
+    public Passenger(int s, int e) {
+        this.s = s;
+        this.e = e;
+    }
+
+    @Override
+    public int compareTo(Passenger o) {
+        if (this.e == o.e) return this.s - o.s;
+        return this.e - o.e;
+    }
+}
