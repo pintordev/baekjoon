cc22ed2dc0156e14918214e38c88ae24a3b6af53
pintor.dev <pintor.dev@gmail.com>
2024-12-23 18:52:10 +0900

    [Gold V] Title: 수 이어 쓰기 2, Time: 104 ms, Memory: 14048 KB -BaekjoonHub

---
+++ 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int n = read();
+        int k = read();
+
+        long len = 1;
+        long cnt = 9;
+        while (k > len * cnt) {
+            k -= len * cnt;
+            len++;
+            cnt *= 10;
+        }
+        k -= 1;
+
+        long num = (int) Math.pow(10, len - 1) + k / len;
+        if (num > n) System.out.println(-1);
+        else System.out.println(String.valueOf(num).charAt((int) (k % len)));
+    }
+
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
