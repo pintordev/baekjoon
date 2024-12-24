6f8ce63973dec9f11aa4ce36f0b72b74f425ae0a
pintor.dev <pintor.dev@gmail.com>
2024-12-24 18:57:00 +0900

    [Gold IV] Title: 괄호, Time: 152 ms, Memory: 13964 KB -BaekjoonHub

--- 백준/Gold/10422. 괄호/괄호.java
+++ 백준/Gold/10422. 괄호/괄호.java
@@ -4,26 +4,22 @@ public class Main {
     public static int mod = 1_000_000_007;
 
     public static void main(String[] args) throws IOException {
-        int t = read();
-
-        StringBuilder sb = new StringBuilder();
-        while (t-- > 0) {
-            sb.append(count(read())).append('\n');
-        }
-        System.out.print(sb);
-    }
-
-    public static long count(int len) {
-        if (len % 2 == 1) return 0;
-
-        long[] dp = new long[len + 1];
-        dp[0] = 1;
-        for (int i = 2; i <= len; i += 2) {
-            for (int j = 2; j <= i; j += 2) {
-                dp[i] = (dp[i] + dp[j - 2] * dp[i - j]) % mod;
+        long[] memo = new long[5001];
+        memo[0] = 1;
+        memo[2] = 1;
+        for (int i = 4; i <= 5000; i += 2) {
+            for (int j = 0; j < i; j += 2) {
+                memo[i] += memo[j] * memo[i - j - 2];
+                memo[i] %= mod;
             }
         }
-        return dp[len];
+
+        int t = read();
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0) {
+            sb.append(memo[read()]).append('\n');
+        }
+        System.out.print(sb);
     }
 
     public static int read() throws IOException {
