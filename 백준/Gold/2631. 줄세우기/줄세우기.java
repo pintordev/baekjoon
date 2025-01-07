60210941d55bc9b30e3c20fa001767202ab72251
pintor.dev <pintor.dev@gmail.com>
2025-01-07 18:44:39 +0900

    [Gold IV] Title: 줄세우기, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int n = read();
+        
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = read();
+        }
+        
+        int[] memo = new int[n];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            memo[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if(arr[i] > arr[j]) memo[i] = Math.max(memo[i], memo[j] + 1);
+            }
+            max = Math.max(max, memo[i]);
+        }
+        System.out.println(n - max);
+    }
+
+    public static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+}
