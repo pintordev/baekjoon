37e5cc3877937b464e96b7675a98532dda5c7384
pintor.dev <pintor.dev@gmail.com>
2025-03-13 21:36:35 +0900

    [level 3] Title: 베스트앨범, Time: 1.02 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] genres, int[] plays) {
+
+        Map<String, Integer> genMap = new HashMap<>();
+        for (int i = 0; i < genres.length; i++) {
+            genMap.put(genres[i], genMap.getOrDefault(genres[i], 0) + plays[i]);
+        }
+
+        int[][] info = new int[genres.length][3];
+        for (int i = 0; i < genres.length; i++) {
+            info[i] = new int[]{genMap.get(genres[i]), i, plays[i]};
+        }
+
+        Arrays.sort(info, (o1, o2) -> {
+            if (o1[0] == o2[0] && o1[2] == o2[2]) return o1[1] - o2[1];
+            else if (o1[0] == o2[0]) return o2[2] - o1[2];
+            return o2[0] - o1[0];
+        });
+
+        int[] result = new int[genres.length];
+        int count = 0, now = 0, idx = 0;
+        for (int i = 0; i < info.length; i++) {
+            if (info[i][0] != now) {
+                now = info[i][0];
+                count = 1;
+            } else count++;
+            if (count <= 2) result[idx++] = info[i][1];
+        }
+
+        return Arrays.copyOf(result, idx);
+    }
+}
