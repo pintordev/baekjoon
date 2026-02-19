f49f3ace660faa5521ab7e207a94e4e5bf7fab4d
pintor.dev <pintor.dev@gmail.com>
2026-02-19 20:26:30 +0900

    [Bronze III] Title: 알람 시계, Time: 168 ms, Memory: 18556 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int hour = scanner.nextInt();
+        int minute = scanner.nextInt();
+
+        minute -= 45;
+        if (minute < 0) {
+            hour--;
+            minute += 60;
+        }
+        if (hour < 0) hour += 24;
+
+        System.out.println(hour + " " + minute);
+    }
+}
